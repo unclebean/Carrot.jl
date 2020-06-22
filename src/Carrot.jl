ddbd93467fd9af40a6cb98da0b3e5f9947b8f600
hou jian <unclebean2013@gmail.com>
2020-06-23 00:34:59 +0800

    add launch command function

--- src/Carrot.jl
+++ src/Carrot.jl
@@ -1,5 +1,18 @@
 module Carrot
 
-# Write your package code here.
+@enum Features batchBot forexPredict web
+
+function launch(feature::String)
+    if feature == string(batchBot)
+        return "batch bot"
+    elseif feature == string(forexPredict)
+        return "forex predict"
+    elseif feature == string(web)
+        return "web"
+    else
+        return "unknown"
+    end
+end
+
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,9 @@ using Carrot
 using Test
 
 @testset "Carrot.jl" begin
+    @test "batch bot" == Carrot.launch("batchBot")
+    @test "forex predict" == Carrot.launch("forexPredict")
+    @test "web" == Carrot.launch("web")
+    @test "unknown" == Carrot.launch("balabala")
     # Write your tests here.
 end

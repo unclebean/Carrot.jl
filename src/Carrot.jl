655866b081f9a3c0dc9199099500944c07c39864
hou jian <unclebean2013@gmail.com>
2020-06-23 23:22:34 +0800

    refactor launch carrot

--- src/Carrot.jl
+++ src/Carrot.jl
@@ -2,17 +2,33 @@ module Carrot
 
 @enum Features batchBot forexPredict web
 
-function launch(feature::String)
-    if feature == string(batchBot)
-        return "batch bot"
-    elseif feature == string(forexPredict)
-        return "forex predict"
-    elseif feature == string(web)
-        return "web"
-    else
-        return "unknown"
+function get_feature_mappings()::Dict
+    feature_map = Dict()
+    for (index, feature) in enumerate(instances(Features))
+        feature_map[string(feature)] = index - 1
     end
+    return feature_map
 end
 
+function launch(command::String)
+    features = get_feature_mappings()
+    if haskey(features, command)
+        feature = features[command]
+        return launch(Val(Features(feature)))
+    end
+    return "unknown"
+end
+
+function launch(::Val{batchBot})
+    return "batch bot"
+end
+
+function launch(::Val{forexPredict})
+    return "forex predict"
+end
+
+function launch(::Val{web})
+    return "web"
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,13 @@
 using Carrot
 using Test
 
-@testset "Carrot.jl" begin
+@testset "launch" begin
     @test "batch bot" == Carrot.launch("batchBot")
     @test "forex predict" == Carrot.launch("forexPredict")
     @test "web" == Carrot.launch("web")
     @test "unknown" == Carrot.launch("balabala")
-    # Write your tests here.
+end
+
+@testset "feature_mappings" begin
+    @test Carrot.get_feature_mappings()["batchBot"] == 0
 end
